96d6e3485c5eab6d0b15bc40792a7f32c68fd51f
Flychuban <kaloanas07@gmail.com>
2025-05-02 09:19:38 +0300

    upload img and automatically refresh

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,16 +1,25 @@
+"use client";
+
 import { SignInButton,SignUpButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import { useRouter } from 'next/navigation';
+import { UploadButton } from "~/utils/uploadthing";
 
 
 export function TopNav() {
+  const router = useRouter();
+
   return (
     <nav className="flex w-full items-center justify-between p-4 border-b text-xl font-semibold">
       <div>Gallery</div>
 
-      <div>
+      <div className='flex flex-row'>
         <SignedOut>
             <SignInButton />
         </SignedOut>
         <SignedIn>
+            <UploadButton endpoint="imageUploader" onClientUploadComplete={() => {
+                router.refresh()
+            }}/>
             <UserButton />
         </SignedIn>  
       </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "~/styles/globals.css";
+import "@uploadthing/react/styles.css";
 import { TopNav } from "~/app/_components/topnav";
 
 import { type Metadata } from "next";
