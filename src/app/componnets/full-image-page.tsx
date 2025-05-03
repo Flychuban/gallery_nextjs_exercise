f8276340074e9207946f3065aea5b403cf24a39e
Flychuban <kaloanas07@gmail.com>
2025-05-03 18:51:57 +0300

    Refactor FullImageView to enhance layout and styling, updating image display and adding image name section. Modify Modal component to remove close button and ensure text color is white for better visibility.

--- src/app/componnets/full-image-page.tsx
+++ src/app/componnets/full-image-page.tsx
@@ -2,5 +2,15 @@ import { getImageById } from "~/server/queries";
 
 export default async function FullImageView(props: {id: number}) {
     const image = await getImageById(props.id);
-    return <img src={image.url} alt="Modal Image" className="w-96"/>
+    return (
+        <div className="flex w-full h-full min-w-0">
+            <div className="flex-shrink flex justify-center items-center">
+                 <img src={image.url} alt="Modal Image" className="object-contain"/>
+            </div>
+            <div className="w-48 flex flex-col flex-shrink-0 flex-col border-l">
+                <div className="text-xl font-bold">{image.name}</div>
+            </div>
+        </div>  
+
+    )
 }
