b8e4bf8450595adb4b6d8492547463958f95c9d7
Flychuban <kaloanas07@gmail.com>
2025-05-05 22:44:38 +0300

    Refactor GlobalError component layout, removing unnecessary HTML tags. Add SimpleUploadButton component for file uploads, integrating with the upload functionality. Update TopNav to use SimpleUploadButton instead of UploadButton for improved code clarity. Enhance upload utility by exporting useUploadThing hook for better integration.

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,25 +1,18 @@
-"use client";
-
 import { SignInButton,SignUpButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
-import { useRouter } from 'next/navigation';
-import { UploadButton } from "~/utils/uploadthing";
+import SimpleUploadButton from './simple-upload-button';
 
 
 export function TopNav() {
-  const router = useRouter();
-
   return (
     <nav className="flex w-full items-center justify-between h-22 py-0 px-4 border-b text-base font-semibold">
       <div>Gallery</div>
 
-      <div className='flex flex-row'>
+      <div className='flex flex-row gap-6 items-center'>
         <SignedOut>
             <SignInButton />
         </SignedOut>
         <SignedIn>
-            <UploadButton endpoint="imageUploader" onClientUploadComplete={() => {
-                router.refresh()
-            }}/>
+            <SimpleUploadButton />
             <UserButton />
         </SignedIn>  
       </div>
